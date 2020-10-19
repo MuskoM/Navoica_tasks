e4e6b7916e4f5cb01c3a32065e4f1f4d474b5162
Mat Musko <muskomatt@outlook.com>
2020-10-19 12:44:34 +0200

    Trying to do exercise 1 Module 2.2

--- src/App.js
+++ src/App.js
@@ -4,55 +4,69 @@ import {Card, Paper} from "@material-ui/core"
 import IncrementButton from './IncrementButton';
 import ValidationComponent from './Validator';
 
-const PrintAge = (props) => {
-  return <span>Age: {age(props) } </span>
-}
 
-const age = (birthDate) =>
-{
-    const months = {
-      "january": 1,
-      "february": 2,
-      "march": 3,
-      "april": 4,
-      "may": 5,
-      "june": 6,
-      "july": 7,
-      "august": 8,
-      "september":9,
-      "october": 10,
-      "november":11,
-      "december":12,
+
+class Person extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name : props.name,
+      surname : props.surname,
+      age: this.age(props)
     }
+  }
 
-    let monthNumber = months[birthDate.month]
+  age = (birthDate) =>
+  {
+      const months = {
+        "january": 1,
+        "february": 2,
+        "march": 3,
+        "april": 4,
+        "may": 5,
+        "june": 6,
+        "july": 7,
+        "august": 8,
+        "september":9,
+        "october": 10,
+        "november":11,
+        "december":12,
+      }
+  
+      let monthNumber = months[birthDate.month]
+  
+      let dateOfBirth = new Date(birthDate.year,monthNumber-1,birthDate.day);
+      let today = new Date();
+      let tempdate = new Date(dateOfBirth.getFullYear(),today.getMonth(),today.getDate(),today.getHours());
+      
+      return (tempdate>=dateOfBirth?today.getFullYear() - dateOfBirth.getFullYear() : today.getFullYear() - dateOfBirth.getFullYear() - 1);
+  }
 
-    let dateOfBirth = new Date(birthDate.year,monthNumber-1,birthDate.day);
-    let today = new Date();
-    let tempdate = new Date(dateOfBirth.getFullYear(),today.getMonth(),today.getDate(),today.getHours());
-    
-    return (tempdate>=dateOfBirth?today.getFullYear() - dateOfBirth.getFullYear() : today.getFullYear() - dateOfBirth.getFullYear() - 1);
+  PrintAge = (props) => {
+    return <span>Age: {this.age(props) } </span>
+  }
+  
+  
+
+  render(){
+    return (
+      <Card style={{backgroundColor:"grey", width:"50%", textAlign:"center"}}>
+        <Paper style={{backgroundColor:"lightgrey", margin:"20px"}}>
+        <div style={{marginLeft:"10px"}}>
+        <p><b> {this.state.name} {this.state.surname}</b></p>
+        <p>Born: {this.state.day} {this.state.month} {this.state.year}</p>
+        {this.PrintAge(this.state)};
+        </div> 
+        </Paper> 
+      </Card>
+    )
+  }
+
+
 }
 
-const Person = props => {
-  return (
-    <Card style={{backgroundColor:"grey", width:"50%", textAlign:"center"}}>
-      <Paper style={{backgroundColor:"lightgrey", margin:"20px"}}>
-      <div style={{marginLeft:"10px"}}>
-      <p><b> {props.name} {props.surname}</b></p>
-      <p>Born: {props.day} {props.month} {props.year}</p>
-	    <p><PrintAge day={props.day} month={props.month} year={props.year}/> years old</p>
-      </div> 
-      </Paper> 
-    </Card>
-  )
-}
 
-PrintAge.propTypes = {
-  year: PropTypes.number.isRequired,
-  month: PropTypes.string,
-  day: PropTypes.number
-}
 
 
 Person.propTypes = { 
---
+++ src/IntroducePerson.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IntroducePerson = (props) =>{
+
+return(
+    if(props.wiek)
+)
+
+}
+
+export default IntroducePerson;
